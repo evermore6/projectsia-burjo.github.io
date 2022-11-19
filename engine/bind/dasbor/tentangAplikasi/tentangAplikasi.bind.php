df9cad7abd1bf86cf423ff05b1e5815e97f32b46
Ahmad Naja <najanajmudin12@gmail.com>
2022-11-19 22:16:03 +0700

    Upload Data
    File awal

---
+++ engine/bind/dasbor/tentangAplikasi/tentangAplikasi.bind.php
@@ -0,0 +1,77 @@
+<div class="row" id="divTentangAplikasi">
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="card card-primary">
+            <div class="card-header"><h4>Kelompok 7</h4></div>
+            <div class="card-body">
+            <ul class="list-unstyled list-unstyled-border">
+                    <li class="media">
+                      <img src="<?=STYLEBASE; ?>/dasbor/img/devTeam/hasnah_ardita.jpg" class="mr-3 rounded-circle" width="50" src="" alt="Hasnah">
+                      <div class="media-body">
+                        <div class="float-right text-primary">7211421093</div>
+                        <div class="media-title">Windi Pramesti </div>
+                        <span class="text-small text-muted">@gmail.com</span>
+                      </div>
+                    </li>
+                    <li class="media">
+                      <img src="<?=STYLEBASE; ?>/dasbor/img/devTeam/adit.jpg" class="mr-3 rounded-circle" width="50" src="" alt="Adit">
+                      <div class="media-body">
+                        <div class="float-right text-primary">7211421224</div>
+                        <div class="media-title">Wanda Nur Halimah</div>
+                        <span class="text-small text-muted">@gmail.com</span>
+                      </div>
+                    </li>
+                    <li class="media">
+                      <img src="<?=STYLEBASE; ?>/dasbor/img/devTeam/anna.jpg" class="mr-3 rounded-circle" width="50" src="" alt="Anna">
+                      <div class="media-body">
+                        <div class="float-right text-primary">7211421358</div>
+                        <div class="media-title">Ahmad Najmudin Alfatih</div>
+                        <span class="text-small text-muted">@gmail.com</span>
+                      </div>
+                    </li>
+                    <li class="media">
+                      <img src="<?=STYLEBASE; ?>/dasbor/img/devTeam/adam.jpg" class="mr-3 rounded-circle" width="50" src="" alt="Adam">
+                      <div class="media-body">
+                        <div class="float-right text-primary">7211421210</div>
+                        <div class="media-title">Karunia Nur Apriyanti</div>
+                        <span class="text-small text-muted">@gmail.com</span>
+                      </div>
+                    </li>
+                  </ul>
+            </div>
+        </div>
+
+        <div class="card card-primary">
+            <div class="card-header"><h4>Daftar Kontributor</h4></div>
+            <div class="card-body">
+                <p id='divKontributor'>
+                    Terima kasih kepada Pak Atta Putra Harjanto yang telah memberikan kami kelompok 7 dalam memberikan bantuan dalam bentuk apapun. Sedikit banyaknya bantuan dari teman teman kelompok lainnya , 
+                    dalam memberikan ilmu dan saran lainnya sehingga kami dapat menyelesaikan project tugas sistem informasi akuntansi ini. 
+                    <a href=https://akuntansi.unnes.ac.id/' target="_new"><h4>Akuntansi UNNES (2022)</h4></a> 
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="card card-primary">
+            <div class="card-header"><h4>Tentang Aplikasi</h4></div>
+            <div class="card-body">
+                <div style="text-align: center;margin-bottom:15px;">
+                <img src="<?=STYLEBASE; ?>/login/images/download.png" style="width: 200px;">
+                </div>
+                <p style="text-align: justify;">
+                <strong>Burjo POS</strong> adalah salah satu project aplikasi yang dibuat untuk memenuhi tugas akhir pada mata kuliah SISTEM INFORMASI AKUNTANSI.
+                Kami berharap agar aplikasi ini dapat digunakan secara menyeluruh bagi pelaku UMKM disekitar agar pelaku usaha dapat menjalankan maupun mengikuti proses digitalisasi dalam usaha mereka agar dapat berkembang. 
+                Kami juga akan selalu menerima kritik & saran dari teman teman baik dari pemilik usaha ataupun yang lain yang mungkin bisa memberikan 
+                masukan atau saran kepada kami. Aplikasi ini sepenuhnya berlisensi opensource (MIT) yang artinya dapat digunakan secara gratis, 
+                serta bagi teman teman yang ingin membantu pengembangan lebih lanjut, juga kami persilahkan untuk membedah & memperbaiki aplikasi yang kami kembangkan.
+                </p>
+            </div>
+    </div>
+    </div>
+
+</div>
+
+<script src="<?=STYLEBASE; ?>/dasbor/tentangAplikasi.js"></script> 
